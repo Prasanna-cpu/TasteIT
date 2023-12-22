2084ba2f44349d20a8a4c905b5e2b6bd9dfc74c4
Prasanna Kumar <kumar22maran@gmail.com>
2023-12-22 13:08:22 +0530

    Added bunch of M/W s and Database Connection API

--- server/index.js
+++ server/index.js
@@ -4,24 +4,30 @@ import mongoose from 'mongoose'
 import {resolve} from 'path'
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+import connectDB from './Database/Connection.js';
 import dotenv from 'dotenv'
+import { UserRouter } from './Routes/Users.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const envPath = resolve(__dirname, "../.env");
 dotenv.config({path:envPath})
+
 const port=process.env.PORT
+const uri=process.env.URI
 
 const app=express()
 app.use(express.json())
 app.use(cors())
-
+app.use("/auth",UserRouter)
 
 app.get("/",(_,res)=>{
     res.send('Hey fellas')
 })
 
+
+connectDB(uri)
 
 app.listen(port,()=>console.log(`Server running at ${port}`))
 
